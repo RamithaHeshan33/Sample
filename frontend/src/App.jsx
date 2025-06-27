141c50378f1413e563e953cd7d7a1aeb4b18b00c
Ramitha Heshan <ramithacampus@gmail.com>
2025-06-27 12:58:50 +0530

    project done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/hello')
+      .then(res => res.text())
+      .then(setMessage);
+  }, []);
+
+  return (
+    <div>
+      <h1>{message || 'Loading...'}</h1>
+    </div>
+  );
+}
+
+export default App;
